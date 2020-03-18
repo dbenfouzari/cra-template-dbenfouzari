9bfe1e620ff670f2ee61aead0cd53ba17676ac10
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2020-03-18 11:30:03 +0100

    :sparkles: implement storybook

---
+++ src/components/button/button.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface ButtonProps {
+  children: React.ReactNode;
+  onClick?: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, onClick }) => (
+  <button style={styles} onClick={onClick}>{children}</button>
+);
+
+const styles = {
+  border: '1px solid #eee',
+  borderRadius: 3,
+  backgroundColor: '#FFFFFF',
+  cursor: 'pointer',
+  fontSize: 15,
+  padding: '3px 10px',
+  margin: 10
+};
+
+export default Button;
