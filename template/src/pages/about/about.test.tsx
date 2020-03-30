5898c7d8a3906e8ff01a15775f4839cebe6e7eae
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2020-03-30 14:47:41 +0200

    :sparkles: templatize app

--- template/src/pages/about/about.test.tsx
+++ template/src/pages/about/about.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import About from './about';
+import { Provider } from 'react-redux';
+import store from 'modules/store';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<About />);
+  const { getByText } = render(
+    <Provider store={store}>
+      <About />
+    </Provider>,
+  );
   const linkElement = getByText(/about/i);
   expect(linkElement).toBeInTheDocument();
 });
